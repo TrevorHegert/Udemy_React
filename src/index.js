94968bfb79d15d29baea47ef70ff909e9f680b99
Trevor Hegert <thegert138@gmail.com>
2019-02-22 13:59:35 -0500

    10

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a React Component
+
+
+//Take the react component and show it on the screen
